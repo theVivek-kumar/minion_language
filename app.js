acf18a98a10b61ee1d0f1b4d20516ae34ca48a34
Vivek-jpg <kumarvivekazio@gmail.com>
2021-08-17 03:15:41 +0530

    second comit

--- app.js
+++ app.js
@@ -1,5 +1,4 @@
 const translateBtn = document.querySelector("#btn-translate");
-const clickYes =document.querySelector("#click");
 const clearBtn = document.querySelector("#btn-clear");
 const input = document.querySelector(".in-textarea");
 const output = document.querySelector(".out-textarea");
@@ -7,7 +6,7 @@ const output = document.querySelector(".out-textarea");
 translateBtn.addEventListener('click',function onClick(){
     const inputText = input.value;
     const url = "https://api.funtranslations.com/translate/minion.json"+"?"+"text="+inputText;
-    console.log("yes i am fan"+inputText)
+   
      fetch(url)
     .then(response => response.json())
     .then(json => output.innerHTML=json.contents.translated)
