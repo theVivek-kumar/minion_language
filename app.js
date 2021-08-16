46d177b07961b28968e23640e0540b5e04780156
Vivek-jpg <kumarvivekazio@gmail.com>
2021-08-17 00:43:50 +0530

    input output processing is done

--- app.js
+++ app.js
@@ -1,2 +1,23 @@
+var btnTranslate=document.querySelector("#btn-translate");
+var txtInput=document.querySelector("#txt-input");
+var outputDiv= document.querySelector("#output");
+
+console.log(txtInput)
+
+function clickHandler(){
+   outputDiv
+};
+
+btnTranslate.addEventListener("click", clickHandler)
+   
+
+
+
+
+
+
+
+ 
+
 
 
